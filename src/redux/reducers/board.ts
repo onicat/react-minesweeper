a6da752705a97459c9ce562cd92fec63222e7f7c
Alexey Onikov <orbenaixasej@gmail.com>
2020-08-06 22:13:56 +0300

    Changed board reducer shape with immer.js

--- src/redux/reducers/board.ts
+++ src/redux/reducers/board.ts
@@ -1,11 +1,18 @@
+import { produce } from 'immer'
+
 import BoardCreator from 'logic/creators/BoardCreator'
+import Board from 'models/Board';
+import { initialSettings } from 'logic/constants';
 
 const boardCreator = new BoardCreator();
-const initialState = boardCreator.create(8, 8);
+const initialState: Board = boardCreator.create(
+  initialSettings.BOARD_WIDTH,
+  initialSettings.BOARD_HEIGHT
+);
 
 
-const board = (state = initialState) => {
-  return state;
-};
+const board = produce((state, action) => {
+
+}, initialState);
 
 export default board;
