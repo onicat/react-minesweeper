c479075a66f6bf91209d324c15780e32757a09dc
Alexey Onikov <orbenaixasej@gmail.com>
2020-08-12 10:16:46 +0300

    Changed StyledCell style

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -6,7 +6,8 @@ import Cell from 'models/Cell'
 const StyledCell = styled.td`
   width: 30px;
   height: 30px;
-  background-color: black
+  background-color: #2196f3;
+  box-shadow: inset 0 -3px #1976d2;
 `;
 
 interface CellComponentProps {
