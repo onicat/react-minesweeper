dfe7c080069704ff5e6ed6a712cb197a0747988f
Alexey Onikov <orbenaixasej@gmail.com>
2020-08-12 10:06:31 +0300

    Created StyledCell

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import Cell from 'models/Cell'
+
+const StyledCell = styled.td`
+  width: 30px;
+  height: 30px;
+  background-color: black
+`;
 
 interface CellComponentProps {
   cell: Cell
@@ -10,7 +17,7 @@ const CellComponent = ({
   cell
 }: CellComponentProps) => {
   return (
-    <td></td>
+    <StyledCell></StyledCell>
   )
 }
 
