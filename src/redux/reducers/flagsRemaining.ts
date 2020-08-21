f5b0a33f87f59915c0f72f3735913897d4dd6f88
Alexey Onikov <orbenaixasej@gmail.com>
2020-08-21 19:40:06 +0300

    Revert "Created changeRemainingFlagsCounter action handler in flagsRemaining"
    This reverts commit bc6a50666ecdc59ad268adcdd92a19eeca507793.

--- src/redux/reducers/flagsRemaining.ts
+++ src/redux/reducers/flagsRemaining.ts
@@ -1,16 +1,10 @@
 import produce from "immer";
-
 import { initialSettings } from "logic/constants";
-import actionTypes from "redux/actionTypes";
 
 const initialState = initialSettings.MINES_NUMBER;
 
 const flagsRemaining = produce((state, action) => {
-  switch (action.type) {
-    case actionTypes.CHANGE_REMAINING_FLAGS_COUNTER: {
-      return action.value;
-    }
-  }
+
 }, initialState);
 
 export default flagsRemaining;
