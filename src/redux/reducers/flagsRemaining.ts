ec01681bb27a280ea27c581bfd9e670a0d3394f8
Alexey Onikov <orbenaixasej@gmail.com>
2020-08-21 19:46:57 +0300

    Created flagsRemainig handlers for action that remove flags

--- src/redux/reducers/flagsRemaining.ts
+++ src/redux/reducers/flagsRemaining.ts
@@ -1,10 +1,19 @@
 import produce from "immer";
 import { initialSettings } from "logic/constants";
+import actionTypes from "redux/actionTypes";
 
 const initialState = initialSettings.MINES_NUMBER;
 
 const flagsRemaining = produce((state, action) => {
+  switch (action.type) {
+    case actionTypes.TOGGLE_FLAG: {
+      return state + ((action.value) ? -1 : 1);
+    }
 
+    case actionTypes.REMOVE_FLAGS: {
+      return state + action.cells.size;
+    }
+  }
 }, initialState);
 
 export default flagsRemaining;
