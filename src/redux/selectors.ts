7ce9e5a292dbe07252ca659d9fbfb99ee60adf60
Alexey Onikov <orbenaixasej@gmail.com>
2020-08-21 19:50:42 +0300

    Created getFlagsRemaining selector

--- src/redux/selectors.ts
+++ src/redux/selectors.ts
@@ -3,3 +3,4 @@ import Store from 'models/Store'
 export const getBoard = (store: Store) => store.board;
 export const getSettings = (store: Store) => store.settings;
 export const getStage = (store: Store) => store.stage;
+export const getFlagsRemaining = (store: Store) => store.flagsRemaining;
