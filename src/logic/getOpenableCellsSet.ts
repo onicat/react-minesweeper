9056c79f74d86396cc6e7cf5d0ae9aaa4b42c2ce
Alexey Onikov <orbenaixasej@gmail.com>
2020-08-21 14:37:18 +0300

    Created getOpenableCellsSet

---
+++ src/logic/getOpenableCellsSet.ts
@@ -0,0 +1,29 @@
+import Cell from "models/Cell";
+import Board from "models/Board";
+import getArea from "./getArea";
+
+const getOpenableCellsSet = (startsWith: Cell, board: Board): Set<Cell> => {
+  const readyToBeChecked: Cell[] = [
+    board[startsWith.rowIndex][startsWith.cellIndex]
+  ];
+  const openableCells: Set<Cell> = new Set(); 
+
+  while (readyToBeChecked.length > 0) {
+    const cell = readyToBeChecked.pop();
+
+    if (cell === undefined) break;
+    if (openableCells.has(cell)) continue;
+
+    if (cell.status === 0) {          
+      const cellArea = getArea(cell, board, false);
+
+      readyToBeChecked.push(...cellArea);
+    }
+
+    openableCells.add(cell);
+  }
+
+  return openableCells;
+};
+
+export default getOpenableCellsSet;
