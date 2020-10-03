ca4c18e97b5008dd2e9db3b107822536034b2f1c
Alexey Onikov <orbenaixasej@gmail.com>
2020-10-03 15:42:28 +0300

    Created changeSettings action handler in settings reducer

--- src/redux/reducers/settings.ts
+++ src/redux/reducers/settings.ts
@@ -2,6 +2,7 @@ import produce from "immer";
 
 import Settings from "models/Settings";
 import { initialSettings } from "logic/constants";
+import actionTypes from "redux/actionTypes";
 
 const initialState: Settings = {
   boardWidth: initialSettings.BOARD_WIDTH,
@@ -10,7 +11,13 @@ const initialState: Settings = {
 };
 
 const settings = produce((state, action) => {
-
+  switch(action.type) {
+    case actionTypes.CHANGE_SETTINGS: {
+      state.boardWidth = action.width;
+      state.boardHeight = action.height;
+      state.minesNumber = action.minesNumber;
+    }
+  }
 }, initialState);
 
 export default settings;
